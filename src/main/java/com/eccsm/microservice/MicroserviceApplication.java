d1f1f64d1dc98576a2491f6ae2f6673c82d2918d
Casim <Ekincan.Casim@bs.nttdata.com>
2022-08-08 14:53:40 +0300

    starter fixed

--- src/main/java/com/eccsm/microservice/MicroserviceApplication.java
+++ src/main/java/com/eccsm/microservice/MicroserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.profiles.active:defdault}.properties")
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
